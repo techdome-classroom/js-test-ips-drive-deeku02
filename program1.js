5f9b527ed62df7283927ae0b9f2a7c31b9ec0afc
Deeksha Jain <95418183+deeku02@users.noreply.github.com>
2024-04-26 12:24:49 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,21 @@
 function smallestMissingPositiveInteger(nums) {
 
+  // Implement the function smallest_missing_positive_integer
+    nums = nums.filter(num => num > 0);
 
-    // Implement the function smallest_missing_positive_integer
-  
-  }
-  
-  module.exports = smallestMissingPositiveInteger;
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if (index < nums.length && nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            return i + 1;
+        }
+    }
+    return nums.length + 1;
 
+}
+
+module.exports = smallestMissingPositiveInteger;
