b85b2ece0e48adcd1055c54a853c359df4e65982
Deeksha Jain <95418183+deeku02@users.noreply.github.com>
2024-04-26 12:23:21 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,29 @@
 function longestSubstring(s) {
-
-
     // Implementation of longestSubstring function
+        let longestSubStringLength = 0;
+    
+        for (let i = 0; i < s.length; i++) {
+            let currentStringSet = new Set();
+    
+            for (let x = i; x < s.length; x++) {
+                
+                if (currentStringSet.has(s[x])) {
+                    
+                    break;
+                } else {
+                    
+                    currentStringSet.add(s[x]);
+                }
+            }
+    
+            longestSubStringLength = Math.max(
+                longestSubStringLength,
+                currentStringSet.size
+            );
+        }
+    
+        return longestSubStringLength;
+    
 }
 
 module.exports = { longestSubstring };
